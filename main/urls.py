675be3640ba4e1818426793ab1efcff5c85c1e33
SergeyLebidko <it.sergeyler@mail.ru>
2021-05-01 16:35:40 +0300

    Создан контроллер для выхода с сайта

--- main/urls.py
+++ main/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import Login, index, inventory
+from .views import Login, Logout, index, inventory
 
 app_name = 'main'
 
 urlpatterns = [
     path('login/', Login.as_view(), name='login'),
+    path('logout/', Logout.as_view(), name='logout'),
     path('index/', index, name='index'),
     path('inventrory/', inventory, name='inventory')
 ]
--- main/views.py
+++ main/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, LogoutView
 
 
 class Login(LoginView):
     template_name = 'main/login.html'
+
+
+class Logout(LogoutView):
+    next_page = reverse_lazy('main:index')
 
 
 def index(request):
