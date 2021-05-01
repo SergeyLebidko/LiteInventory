7e49b540ec6271955d1a2ef189ec0b3e2dacb3fb
SergeyLebidko <it.sergeyler@mail.ru>
2021-05-01 16:14:21 +0300

    Создан каркас для страницы инвентаря

--- main/urls.py
+++ main/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import index
+from .views import index, inventory
 
 app_name = 'main'
 
 urlpatterns = [
-    path('index/', index, name='index')
+    path('index/', index, name='index'),
+    path('inventrory/', inventory, name='inventory')
 ]
--- main/views.py
+++ main/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 
 def index(request):
     return render(request, 'main/index.html', context={})
+
+
+@login_required
+def inventory(request):
+    return render(request, 'main/inventory.html', context={})
