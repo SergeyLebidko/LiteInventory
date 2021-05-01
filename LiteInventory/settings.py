c6fccc4a9e9fbc376c2a48aaa7a8211eacbb2e5d
SergeyLebidko <it.sergeyler@mail.ru>
2021-05-01 14:00:39 +0300

    Установлен и настроен django-cors-headers

--- LiteInventory/settings.py
+++ LiteInventory/settings.py
@@ -39,12 +39,14 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     'main'
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -127,3 +129,6 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# В режиме разработки разрешаем CORS с любых доменов
+CORS_ORIGIN_ALLOW_ALL = DEBUG
