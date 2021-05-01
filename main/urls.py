2cb11e2016b89f5cf1b8873de9a239ab1f594ca3
SergeyLebidko <it.sergeyler@mail.ru>
2021-05-01 18:38:26 +0300

    Добавлен механизм смены пароля

--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import Register, Login, Logout, index, inventory
+from .views import Register, Login, Logout, ChangePassword, ChangePasswordDone, index, inventory
 
 app_name = 'main'
 
@@ -7,6 +7,8 @@ urlpatterns = [
     path('register/', Register.as_view(), name='register'),
     path('login/', Login.as_view(), name='login'),
     path('logout/', Logout.as_view(), name='logout'),
+    path('change_password/', ChangePassword.as_view(), name='change_password'),
+    path('change_password_done/', ChangePasswordDone.as_view(), name='change_password_done'),
     path('index/', index, name='index'),
     path('inventrory/', inventory, name='inventory')
 ]
--- main/views.py
+++ main/views.py
@@ -2,19 +2,11 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.urls import reverse, reverse_lazy
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.views import LoginView, LogoutView, PasswordChangeView, PasswordChangeDoneView
 from django.views.generic.base import View
 from django.contrib.auth import login
 
 from .forms import RegisterForm
-
-
-class Login(LoginView):
-    template_name = 'main/login.html'
-
-
-class Logout(LogoutView):
-    next_page = reverse_lazy('main:index')
 
 
 class Register(View):
@@ -27,10 +19,29 @@ class Register(View):
         form = RegisterForm(request.POST)
         if form.is_valid():
             user = form.save()
+            user.set_password(request.POST['password'])
+            user.save()
             login(request, user)
             return HttpResponseRedirect(reverse('main:inventory'))
 
         return render(request, 'main/register.html', context={'form': form})
+
+
+class Login(LoginView):
+    template_name = 'main/login.html'
+
+
+class Logout(LogoutView):
+    next_page = reverse_lazy('main:index')
+
+
+class ChangePassword(PasswordChangeView):
+    template_name = 'main/change_password.html'
+    success_url = reverse_lazy('main:change_password_done')
+
+
+class ChangePasswordDone(PasswordChangeDoneView):
+    template_name = 'main/change_password_done.html'
 
 
 def index(request):
