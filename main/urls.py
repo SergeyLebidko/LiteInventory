135c66c94b3bfe8989430c8c7b7e57aa0f508372
SergeyLebidko <it.sergeyler@mail.ru>
2021-05-01 16:27:22 +0300

    Создана страница входа

--- main/urls.py
+++ main/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import index, inventory
+from .views import Login, index, inventory
 
 app_name = 'main'
 
 urlpatterns = [
+    path('login/', Login.as_view(), name='login'),
     path('index/', index, name='index'),
     path('inventrory/', inventory, name='inventory')
 ]
--- main/views.py
+++ main/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.views import LoginView
+
+
+class Login(LoginView):
+    template_name = 'main/login.html'
 
 
 def index(request):
