68fb9545b20446255e7f3bf2a1881ef8ede5dea8
SergeyLebidko <it.sergeyler@mail.ru>
2021-05-02 15:32:40 +0300

    Создана модель ResetPasswordCode

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import ResetPasswordCode
+
+
+@admin.register(ResetPasswordCode)
+class ResetPasswordCodeAdmin(admin.ModelAdmin):
+    list_display = ['user', 'code', 'uuid']
--- main/models.py
+++ main/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class ResetPasswordCode(models.Model):
+    """Модель для хранения кодов сброса пароля"""
+    user = models.ForeignKey(User, verbose_name='Пользователь', on_delete=models.CASCADE)
+    code = models.CharField(verbose_name='Код', max_length=10)
+    uuid = models.CharField(verbose_name='UUID', max_length=36)
+
+    def __str__(self):
+        return f'Код для сброса пароля пользователя {self.user}'
+
+    class Meta:
+        verbose_name = 'Код сброса паролей'
+        verbose_name_plural = 'Коды сброса паролей'
