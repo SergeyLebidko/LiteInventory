ee39151e5465bd55cd20b8a76f88b3186b73029d
SergeyLebidko <it.sergeyler@mail.ru>
2021-05-02 18:14:00 +0300

    В модель ResetPasswordCode добавлены условия уникальности

--- main/models.py
+++ main/models.py
@@ -5,8 +5,8 @@ from django.contrib.auth.models import User
 class ResetPasswordCode(models.Model):
     """Модель для хранения кодов сброса пароля"""
     user = models.ForeignKey(User, verbose_name='Пользователь', on_delete=models.CASCADE)
-    code = models.CharField(verbose_name='Код', max_length=10)
-    uuid = models.CharField(verbose_name='UUID', max_length=36)
+    code = models.CharField(verbose_name='Код', max_length=10, unique=True)
+    uuid = models.CharField(verbose_name='UUID', max_length=36, unique=True)
 
     def __str__(self):
         return f'Код для сброса пароля пользователя {self.user}'
