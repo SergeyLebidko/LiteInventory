69c72a56ef8dd1a31a7755bf637f37cabed6a123
SergeyLebidko <it.sergeyler@mail.ru>
2021-05-03 19:10:31 +0300

    Добавлена модель Token

--- api/models.py
+++ api/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Token(models.Model):
+    token = models.CharField(verbose_name='Токен', max_length=36)
+    user = models.ForeignKey(User, verbose_name='Пользователь', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'Токен пользователя {self.user}'
+
+    class Meta:
+        verbose_name = 'Токен пользователя'
+        verbose_name_plural = 'Токены пользователей'
