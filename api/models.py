9c54e2a456e85bee9d0ec8a9e81cf67bc613ea2b
SergeyLebidko <it.sergeyler@mail.ru>
2021-05-04 17:36:53 +0300

    В модель Token добавлено ограничение уникальности

--- api/models.py
+++ api/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 class Token(models.Model):
     """Токены пользователей для аутентификации запросов к API"""
 
-    token = models.CharField(verbose_name='Токен', max_length=36)
+    token = models.CharField(verbose_name='Токен', max_length=36, unique=True)
     user = models.ForeignKey(User, verbose_name='Пользователь', on_delete=models.CASCADE)
 
     def __str__(self):
