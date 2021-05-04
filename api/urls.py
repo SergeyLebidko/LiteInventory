f7c58fe6dfe0831c872c9169d58b37dc9951ccbb
SergeyLebidko <it.sergeyler@mail.ru>
2021-05-04 19:03:17 +0300

    Создан хук для регистрации пользователей

--- api/urls.py
+++ api/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import login, logout
+from .views import login, logout, register
 
 app_name = 'api'
 
 urlpatterns = [
     path('login/', login, name='login'),
-    path('logout/', logout, name='logout')
+    path('logout/', logout, name='logout'),
+    path('register/', register, name='register')
 ]
