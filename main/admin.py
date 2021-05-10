a93ad32e18294daeed44e6ff26b06102bb23893f
SergeyLebidko <it.sergeyler@mail.ru>
2021-05-10 16:40:22 +0300

    Созданы модели для хранения данных об оборудовании

--- main/admin.py
+++ main/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import ResetPasswordCode, Group
+from .models import ResetPasswordCode, Group, EquipmentCard, EquipmentType, EquipmentFeature
 
 
 @admin.register(ResetPasswordCode)
@@ -11,3 +11,27 @@ class ResetPasswordCodeAdmin(admin.ModelAdmin):
 @admin.register(Group)
 class GroupAdmin(admin.ModelAdmin):
     list_display = ['user', 'group', 'title']
+    list_display_links = ['title']
+
+
+@admin.register(EquipmentCard)
+class EquipmentCardAdmin(admin.ModelAdmin):
+    list_display = ['equipment_user', 'group', 'equipment_type', 'title']
+    list_display_links = ['equipment_type', 'title']
+
+    def equipment_user(self, rec):
+        return rec.user
+
+    equipment_user.short_description = 'Пользователь'
+
+
+@admin.register(EquipmentType)
+class EquipmentTypeAdmin(admin.ModelAdmin):
+    list_display = ['user', 'title']
+    list_display_links = ['title']
+
+
+@admin.register(EquipmentFeature)
+class EquipmentFeatureAdmin(admin.ModelAdmin):
+    list_display = ['name', 'value']
+    list_display_links = ['name', 'value']
--- main/models.py
+++ main/models.py
@@ -38,3 +38,42 @@ class Group(models.Model):
         verbose_name = 'Группа'
         verbose_name_plural = 'Группы'
         ordering = ['title']
+
+
+class EquipmentCard(models.Model):
+    """Модель для хранения учетных карточек оборудования"""
+
+    group = models.ForeignKey(Group, verbose_name='Группа', on_delete=models.CASCADE)
+    inv_number = models.CharField(max_length=100, verbose_name='Инвентарный номер', null=True, blank=True)
+    equipment_type = models.ForeignKey('EquipmentType', verbose_name='Тип оборудования', on_delete=models.CASCADE)
+    title = models.CharField(max_length=1000, verbose_name='Описание', null=True, blank=True)
+    comment = models.TextField(verbose_name='Комментарий', null=True, blank=True)
+    worker = models.CharField(max_length=1000, verbose_name='Пользователь оборудования', null=True, blank=True)
+    purchase_date = models.DateField(verbose_name='Дата приобретения', null=True, blank=True)
+    price = models.DecimalField(max_digits=15, decimal_places=2, verbose_name='Стоимость', null=True, blank=True)
+
+    class Meta:
+        verbose_name = 'Учетная карточка'
+        verbose_name_plural = 'Учетные карточки'
+
+
+class EquipmentType(models.Model):
+    """Модель для хранения типов оборудования (например, десктоп, ноутбук, сервер и т.д.)"""
+
+    user = models.ForeignKey(User, verbose_name='Пользователь', on_delete=models.CASCADE)
+    title = models.CharField(max_length=100, verbose_name='Наименование')
+
+    class Meta:
+        verbose_name = 'Тип оборудования'
+        verbose_name_plural = 'Типы оборудования'
+
+
+class EquipmentFeature(models.Model):
+    """Модель для хранения характеристик оборудования (например, модель процессора, объем памяти и т.д.)"""
+
+    name = models.CharField(max_length=100, verbose_name='Название характеристики')
+    value = models.CharField(max_length=1000, verbose_name='Значение характеристики')
+
+    class Meta:
+        verbose_name = 'Характеристика оборудования'
+        verbose_name_plural = 'Характеристики оборудования'
